80e1c61d9686e009df273245ab1d3424e346ee90
JADEN123b <chooazah@gmail.com>
2025-02-25 09:32:07 +0100

    my calclator

---
+++ calculator.js
@@ -0,0 +1,24 @@
+const add = function(num1, num2) {
+    return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+    return num1 - num2;
+};
+
+const sum = function(numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+};
+
+const multiply = function(numbers) {
+    return numbers.reduce((product, num) => product * num, 1);
+};
+
+const power = function(base, exponent) {
+    return Math.pow(base, exponent);
+};
+
+const factorial = function(num) {
+    }
+    return num;
+};
